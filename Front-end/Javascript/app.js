b0a5cb06a3b0b0c9a2b58534c4ec7299ab3e7221
Lukman Ahmad <ellukmanahmad67@gmail.com>
2025-05-20 06:31:02 +0700

    loop

--- Front-end/Javascript/app.js
+++ Front-end/Javascript/app.js
@@ -1,2 +1,9 @@
-alert(1+1);
-console.log("hello world");
+const abjad = 'abcd'
+for (let i = 1; i <= 10; i++)
+{
+    console.log(`${i}. Soal nomor`);
+    for (let j = 0; j <= abjad.length; j++)
+    {
+        console.log(`   ${abjad[j]}. Pilihan Jawaban`)
+    }
+}

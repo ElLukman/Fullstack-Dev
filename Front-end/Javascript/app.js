e34fbf63662bf8f1c0d3438b890c5332b50d5c31
ElLukman <ellukmanahmad67@gmail.com>
2025-05-20 09:05:45 +0700

    math

--- Front-end/Javascript/app.js
+++ Front-end/Javascript/app.js
@@ -1,3 +1,4 @@
+// For loops
 const abjad = 'abcd'
 for (let i = 1; i <= 10; i++)
 {
@@ -7,3 +8,65 @@ for (let i = 1; i <= 10; i++)
         console.log(`   ${abjad[j]}. Pilihan Jawaban`)
     }
 }
+
+// Multidimension Array
+const BOARD = [
+    ['A','B','C'],
+    ['A','B','C'],
+    ['A','B','C'],
+]
+
+for (let x = 0; x < BOARD.length; x++)
+{
+    const row = BOARD[x];
+    console.log(`Baris ke-${x+1}`);
+    for (let y = 0; y < BOARD.length; y++)
+    {
+        console.log(`   ${row[y]}`);
+    }
+}
+
+// Another example of multidimension array 
+const BOARD_2 = 
+[
+    ['O','O','X'],
+    ['O','X','O'],
+    ['X','O','O']
+]
+
+for (let board2_x = 0; board2_x < BOARD_2.length; board2_x++)
+{
+    for (let board2_y = 0; board2_y < BOARD_2.length; board2_y++)
+    {
+        console.log(`   ${BOARD_2[board2_y]}`);
+    }
+    console.log(" ");
+}
+
+// While loops password handling example
+/*
+let PASS = 'ADMIN123';
+
+let guess = prompt('Enter the password');
+while (guess !== PASS)
+{
+    guess = prompt('Enter the password');
+}
+alert('Password sudah benar');
+*/
+
+// Break statement for while loops
+/*
+let input = prompt('Enter your input');
+
+while(true)
+{
+    input = prompt(input);
+    if (input.toLocaleLowerCase() === 'stop') break;
+}
+alert('ok');
+*/
+
+// Math Random 
+const RandomNum = Math.floor((Math.random() * 45) + 1);
+console.log(RandomNum);
